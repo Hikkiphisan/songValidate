a043488a2531050a9b6e36b5560b75c92db46077
Hikkiphisan <162010903+Hikkiphisan@users.noreply.github.com>
2024-11-05 15:48:31 +0700

    jhgcvhj

--- src/CrawlSongExample.java
+++ src/CrawlSongExample.java
@@ -1,34 +1,61 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class CrawlSongExample {
-    public static void main(String[] args) {
-        try {
-            URL url = new URL("https://www.nhaccuatui.com/playlist/nhac-nhat-hay-nhat-various-artists.oxf2WDl3Sp5d.html");
-            Scanner scanner= new Scanner(new InputStreamReader(url.openStream()));
-
-            scanner.useDelimiter("\\Z");
-            String content = scanner.next();
-
-            scanner.close();
-            content = content.replaceAll("\\n+","");
-//            Pattern pattern = Pattern.compile("href='/'>(.*?)</a>");
-//            Pattern pattern = Pattern.compile("name_song">(.*?)</a>");
-            Pattern pattern = Pattern.compile("class=\"name_song\">(.*?)</a");
-            Matcher matcher = pattern.matcher(content);
-            while (matcher.find()) {
-                System.out.println(matcher.group(1));
-            }
+public class ElectricPowerSwitch implements Switch {
 
 
-        } catch (IOException o) {
-            o.printStackTrace();
-        }
+    public Switchable client;
+
+
+    public boolean on;
+
+
+    public ElectricPowerSwitch(Switchable client) {
+
+
+        this.client = client;
+
+
+        this.on = false;
+
 
     }
+
+
+    public boolean isOn() {
+
+
+        return this.on;
+
+
+    }
+
+
+    public void press(){
+
+
+        boolean checkOn = isOn();
+
+
+        if (checkOn) {
+
+
+            client.turnOff();
+
+
+            this.on = false;
+
+
+        } else {
+
+
+            client.turnOn();
+
+
+            this.on = true;
+
+
+        }
+
+
+    }
+
+
 }
